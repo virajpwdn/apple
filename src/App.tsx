c5650610f7a9332b869423bc3a2599e68a250467
viraj <virajpatwardhan6@gmail.com>
2026-01-05 08:39:34 +0530

    feat: added ambient lights and spotlights

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import ProductViewer from "./components/ProductViewer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <ProductViewer />
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
