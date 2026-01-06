947a312bdb22a26b65e4d644562bdd4c35edfecb
viraj <virajpatwardhan6@gmail.com>
2026-01-06 07:19:53 +0530

    feat: complete footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,30 @@
+import { footerLinks } from "../constants/navbar-links";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
-export default Footer
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop: Find an Apple Store or other retailer new you.
+          Order now
+        </p>
+        <img src="/logo.svg" alt="Apple Logo" />
+      </div>
+
+      <hr />
+
+      <div className="links">
+        <p>Copyright &copy; 2024 Apple Inc. All rights reserved</p>
+
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
