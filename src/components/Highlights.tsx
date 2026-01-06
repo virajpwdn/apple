310b7430b762e2003689d745a87f1b9df6b1af3f
viraj <virajpatwardhan6@gmail.com>
2026-01-06 07:08:54 +0530

    feat: hightlightes section complete

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -1,6 +1,61 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
+
 const Highlights = () => {
+  const isMobile = useMediaQuery({ query: "max-width: 1024px" });
+
+  useGSAP(() => {
+    gsap.to([".left-column", ".right-column"], {
+      scrollTrigger: {
+        trigger: ".masonary",
+        start: isMobile ? "bottom bottom" : "top top",
+      },
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      stagger: 1,
+      ease: "power1.inOut",
+    });
+  });
   return (
-    <div>Highlights</div>
-  )
-}
-export default Highlights
+    <section id="highlights">
+      <h2>There's never been a better time to upgrade.</h2>
+      <h3>Here's what you get with the new MacBook Pro.</h3>
+
+      <div className="masonry">
+        <div className="left-column">
+          <div>
+            <img src="/laptop.png" alt="laptop" />
+            <p>Fly through demanding tasks up to 9.8x faster</p>
+          </div>
+          <div>
+            <img src="/sun.png" alt="sun" />
+            <p>
+              A Stunning <br /> Liquid Retina XDR <br /> display
+            </p>
+          </div>
+        </div>
+        <div className="right-column">
+          <div className="apple-gradient">
+            <img src="/ai.png" alt="AI" />
+            <p>
+              Built for <br /> <span>Apple Intelligence.</span>
+            </p>
+          </div>
+          <div>
+            <p>
+              {" "}
+              Up to
+              <img src="/battery.png" alt="battery" />
+              <span className="green-gradient"> 14 more hours </span>
+              battery life.
+              <span className="text-dark-100"> (Up to 24 hours total.)</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Highlights;
